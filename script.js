a02fdd69c808d479dd66811843d4d1a76a379273
Eslynyer Z <75596467+Eslynyerz@users.noreply.github.com>
2020-12-07 00:02:08 -0400

    Add files via upload
    Portfolio

---
+++ script.js
@@ -0,0 +1,17 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+z
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+
